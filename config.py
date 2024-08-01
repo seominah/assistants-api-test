ee70f79daaeca2595d6a5893229581617a5817fa
서민아 <minahseo@hansol.com>
2024-08-01 11:23:30 +0900

    Merge source

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     OPEN_API_KEY = os.environ.get('OPEN_API_KEY')
     ASSISTANTS_ID = os.environ.get('ASSISTANTS_ID')
     THREAD_ID = os.environ.get('THREAD_ID')
+    MAX_TOKENS = os.environ.get('MAX_TOKENS')
